e9e2742cc3af2e30341e336654c9d746161e70b7
I071499 <naveen.arundi@sap.com>
2025-02-22 13:16:46 +0530

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Alert from "./components/Alert";
-
+import Button from "./components/Button";
 function App() {
   return (
     <div>
       <Alert>
         Hiiii <span>Naveen</span>
       </Alert>
+      <Button onClick={() => console.log("Pressed")} color="danger">
+        {" "}
+        press me
+      </Button>
     </div>
   );
 }
